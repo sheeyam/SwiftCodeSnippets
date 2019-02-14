9fc2292854fdb0c56b336aad5817f8ac9ab82e7b
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-02-14 15:17:30 -0600

    Create TouchIDVerification.swift

---
+++ TouchIDVerification.swift
@@ -0,0 +1,22 @@
+//Create Local Authentication Context
+let authenticationContext = LAContext()
+        
+var error:NSError?
+guard authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+    print("No Apple TouchID Sensor Has Been Detected. This device does not support Touch Id.")
+    return
+}
+        
+authenticationContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, 
+            localizedReason: "Only device owner is allowed", reply: { (success, error) -> Void in
+            
+            if( success ) {
+                print("Fingerprint recognized. You are a device owner!")
+                //Call you Custom Functions
+            } else {
+                // Check if there is an error
+                if let errorObj = error {
+                    print("Error took place. \(errorObj.localizedDescription)")
+                }  
+            }
+        })

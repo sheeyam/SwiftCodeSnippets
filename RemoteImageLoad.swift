646177d72e79595b5916ad48c3dbe2864b34465e
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-02-28 23:33:00 -0600

    Update RemoteImageLoad.swift

--- RemoteImageLoad.swift
+++ RemoteImageLoad.swift
@@ -1,6 +1,6 @@
 import UIKit
   
-  class ViewController: UIViewController {
+ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -8,29 +8,29 @@ import UIKit
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let imageUrlString = "http://*****/1.jpg"
-        let imageUrl:URL = URL(string: imageUrlString)!
+        let remoteImageUrlString = "http://*****/1.jpg"
+        let remoteImageUrl:URL = URL(string: remoteImageUrlString)!
         
         // Start background thread so that image loading does not make app unresponsive
-         DispatchQueue.global(qos: .userInitiated).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             
-            let imageData:NSData = NSData(contentsOf: imageUrl)!
-            let imageView = UIImageView(frame: CGRect(x:0, y:0, width:200, height:200))
-            imageView.center = self.view.center
+           let imageData:NSData = NSData(contentsOf: remoteImageUrl)!
+           let imageView = UIImageView(frame: CGRect(x:0, y:0, width:200, height:200))
+           imageView.center = self.view.center
             
-            // When from background thread, UI needs to be updated on main_queue
+           // When from background thread, UI needs to be updated on main_queue
            DispatchQueue.main.async {
                 let image = UIImage(data: imageData as Data)
                 imageView.image = image
                 imageView.contentMode = UIViewContentMode.scaleAspectFit
                 self.view.addSubview(imageView)      
-            }
-        }
-    }
+           }
+       }
+   }
     
-    override func didReceiveMemoryWarning() {
+   override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+   }
     
-  }
+}
